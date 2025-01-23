1ea7e749942e907503cc2c1dcd4da8b4cfd82d94
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-01-23 15:06:45 +0100

    cositas!!!

--- angular/formularios/src/app/components/reactive-form/reactive-form.component.ts
+++ angular/formularios/src/app/components/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -20,32 +20,56 @@ export class ReactiveFormComponent {
       email: new FormControl("", [
         Validators.required,
         Validators.email
-        Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)
+        Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)
       ]),
       edad: new FormControl("", [
         Validators.required,
         Validators.min(18),
         Validators.max(100)
       ]),
-      dni: new FormControl("", []),
+      dni: new FormControl("", [
+
+      ]),
       password: new FormControl("", []),
       repitepassword: new FormControl("", []),
-    }[]){
+    }[
+      this.passwordValidator])
 
+  }
+  passwordValidator(formFiels: AbstractControl): any {
+    let password = formFiels.get('password')?.value;
+    let repitepassword = formFiels.get('repitepassword')?.value;
+    return password !== repitepassword ? { 'passwordvalidator': true } : null;
+  }
 
-    }
-
-
-    getDataForm() {
-      console.log(this.formModel.value);
-
-
-    }
-
-
+  getDataForm() {
+    console.log(this.formModel.value);
 
 
   }
+
+  dniValidator(controlName: AbstractControl): any {
+    const letrasDni: string[] = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
+    let dni = controlName.value;
+    let exp = /^d{8}[A-Z]{1}/;
+    if (exp.test(dni)) {
+      let letra = dni.at(-1)
+      const numero = Number(dni.substring(0, dni.length - 1));
+      const posicion = numero % 23;
+      if (letra !== letrasDni[posicion]) {
+        return { 'dnivalidator': 'la letra no coincide' };
+      } else {
+        return null;
+      }
+    }
+    else {
+      return { 'dnivalidator': 'el formato del DNI es incorrecto' };
+    }
+  }
+
+
+
+
   checkErrorField(field: string, error: string): boolean {
     if (this.formModel.get(field).hasError(error)?.hasError(error) && this.formModel.get(field)?.touched) {
       return true;
