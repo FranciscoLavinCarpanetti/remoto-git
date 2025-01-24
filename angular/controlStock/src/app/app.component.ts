b4e43312bdaab8b2a80ddd5e8a9d7a33a3ae4a73
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-01-24 12:52:13 +0100

    función insert

--- angular/controlStock/src/app/app.component.ts
+++ angular/controlStock/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormProductComponent } from "./componets/form-product/form-product.component";
+import { ProductsListComponent } from './componets/products-list/products-list.component';
+import { TotalComponent } from './componets/total/total.component';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormProductComponent, ProductsListComponent, TotalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular/controlStock/src/app/services/products.service.ts
+++ angular/controlStock/src/app/services/products.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { IProduct } from '../interfaces/iproduct.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  private arrProductos: IProduct[] = []
+  private id: number = 1;
 
-  constructor() { }
+
+  insert(newProduct: IProduct): any {
+    let busqueda = this.arrProductos.find(product => product.title.toLocaleLowerCase() === newProduct.title.toLocaleLowerCase());
+    if (busqueda === undefined) {
+      newProduct.id = this.id;
+      this.arrProductos.push(newProduct);
+      this.id++;
+      return { message: "Producto agregado correctamente" };
+    } else {
+      busqueda.quantity += newProduct.quantity;
+      return { message: "Producto actualizado correctamente" };
+    }
+
+
+
+  }
+
+
 }
